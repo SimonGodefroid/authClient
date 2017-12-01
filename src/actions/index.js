c9a46f5a789248c296770814e6a2f029b70708a1
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-01 19:09:28 +0100

    added fetch message action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from './types';
 
 const ROOT_URL = 'http://localhost:5000';
 
@@ -56,4 +56,15 @@ export function authError(error) {
 export function signOutUser() {
 	localStorage.removeItem('token');
 	return { type: UNAUTH_USER };
+}
+
+export function fetchMessage() {
+	return function(dispatch) {
+		axios.get(ROOT_URL, { headers: { authorization: localStorage.getItem('token') } }).then(response =>
+			dispatch({
+				type: FETCH_MESSAGE,
+				payload: response.data.message
+			})
+		);
+	};
 }
--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions/types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from '../actions/types';
 export default function(state = {}, action) {
 	switch (action.type) {
 		case AUTH_USER:
@@ -14,6 +14,8 @@ export default function(state = {}, action) {
 			};
 		case AUTH_ERROR:
 			return { ...state, error: action.payload };
+		case FETCH_MESSAGE:
+			return { ...state, message: action.payload };
 	}
 	return state;
 }
